af6019ffe722edd90d90e944e54891854172a5a4
Tiago Moreira Totti <tiagot@dbserver.com.br>
2017-12-27 11:59:01 -0200

    wihtout build

--- nexus/nexus3-container/usr/local/bin/addjbossrepos.sh
+++ nexus/nexus3-container/usr/local/bin/addjbossrepos.sh
@@ -7,12 +7,12 @@ then
    export NEXUS_BASE_URL="http://localhost:8081"
 fi
 
-${DIR}/nexusscript-create.sh /opt/sonatype/nexus/etc/jboss-public-repo.json
-${DIR}/nexusscript-create.sh /opt/sonatype/nexus/etc/redhat-techpreview-all-repo.json
-${DIR}/nexusscript-create.sh /opt/sonatype/nexus/etc/redhat-ga-repo.json
-${DIR}/nexusscript-create.sh /opt/sonatype/nexus/etc/redhat-group-repo.json
+# ${DIR}/nexusscript-create.sh /opt/sonatype/nexus/etc/jboss-public-repo.json
+# ${DIR}/nexusscript-create.sh /opt/sonatype/nexus/etc/redhat-techpreview-all-repo.json
+# ${DIR}/nexusscript-create.sh /opt/sonatype/nexus/etc/redhat-ga-repo.json
+# ${DIR}/nexusscript-create.sh /opt/sonatype/nexus/etc/redhat-group-repo.json
 
-${DIR}/nexusscript-run.sh jboss-public-repo
-${DIR}/nexusscript-run.sh redhat-techpreview-all-repo
-${DIR}/nexusscript-run.sh redhat-ga-repo
-${DIR}/nexusscript-run.sh redhat-group-repo
+# ${DIR}/nexusscript-run.sh jboss-public-repo
+# ${DIR}/nexusscript-run.sh redhat-techpreview-all-repo
+# ${DIR}/nexusscript-run.sh redhat-ga-repo
+# ${DIR}/nexusscript-run.sh redhat-group-repo
